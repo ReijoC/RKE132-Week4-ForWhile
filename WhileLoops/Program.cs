9a63be5754ebbdcdcf694bd6cf2ae7d6f6147665
Reijo P <rpajussa@tktk.ee>
2023-04-29 11:04:35 +0300

    add week4 code

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿Random rnd  = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.WriteLine($"{cpuRandom}");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congrats, you won");
+        //loopActive = false;
+        break;
+    }
+    else { Console.WriteLine("try again!"); 
+    }
+}
+Console.WriteLine("have a nice day");
